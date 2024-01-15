d7cd7bfb8ab0d1468b971edad8b353b994851aa5
Mythanhthile <93250566+Mythanhthile@users.noreply.github.com>
2024-01-15 17:09:58 +0100

    loooool

--- Eksamen/MyT/Exam1/Exam12/Prog.fs
+++ Eksamen/MyT/Exam1/Exam12/Prog.fs
@@ -2,6 +2,7 @@ module Exam12.Prog
 // For more information see https://aka.ms/fsharp-console-apps
 open System
 open Microsoft.FSharp.Reflection
+
 type Weapon =
     Rock | Paper | Scissors | Lizard | Spock
 
@@ -10,10 +11,14 @@ type Outcome =
     | Lose
     | Tie
 
+type Action =
+    | PlayRound
+    | MaxRounds
+    | Quit
 
 type Score = { Player: int; Computer: int; Rounds: int}
 
-type Percentages =  { PlayerPercentage : float; ComputerPercentage: float}
+type Percentages =  { PlayerPercentage : float; ComputerPercentage: float; TiePercentage: float}
 
 
 
@@ -90,7 +95,7 @@ let updateScore currentScore outcome  =
         match outcome with
         | Win ->  {currentScore with Player = currentScore.Player + 1 }
         | Lose -> { currentScore with Computer = currentScore.Computer + 1 }
-        | Tie -> { currentScore with Player = currentScore.Player + 1; Computer = currentScore.Computer + 1}
+        | Tie -> currentScore
     { updatedScore with Rounds = updatedScore.Rounds + 1 }
 
     
@@ -109,34 +114,78 @@ let printScore score =
 //if total rounds = 5 or esc, end game
 // type should be either round or input
 //print scores and winner
+let tryToInt (input: string) =
+    match Int32.TryParse(input) with
+    | (true, value) -> Some value
+    | _ -> None
 
+type PlayerAction =
+    | Some of Weapon
+    | None
 
-let round (initialScore: Score, random: Random ) =
-    let playerinput =
-        printOptions
-        |> (fun () -> Console.ReadLine() |> int)
-        |> generatePlayerChoice
-        |> playerPick
-    let computerinput = generateComputerChoice random |> computerPick
-    let scoreoutcome = outcome playerinput computerinput
-    let updatedScore = updateScore initialScore scoreoutcome
-    printScore updatedScore
-    updatedScore
+let rec action () =
+    let player = Console.ReadLine()
+    match player with
+    | "q" -> None
+    | _ when tryToInt player |> Option.isSome ->
+        Some ((generatePlayerChoice (tryToInt player |> Option.get)))
+    | _ ->
+        printfn "Enter a valid number between 1 and 5"
+        action ()
+        
+        
+        
+let updateAction total =
+    let maxRounds = 10
+
+    match total with
+    | value when value >= maxRounds -> MaxRounds
+    | _ -> PlayRound
     
 
-let calculatePercentages score totalPossible =
-    let totalGames = float totalPossible
+let playRound initialScore random =
+    let playerAction = action()
+    
+    match playerAction with
+    | None -> Quit, initialScore
+    | Some weapon ->
+        playerPick weapon
+        let computerinput = generateComputerChoice random |> computerPick
+        let scoreoutcome = outcome weapon computerinput
+        let updatedScore = updateScore initialScore scoreoutcome
+        printScore updatedScore
+        let newAction = updateAction updatedScore.Rounds
+        newAction, updatedScore
+        
+
+// let round (initialScore: Score, random: Random ) =
+//     let playerinput =
+//         printOptions
+//         |> (fun () -> Console.ReadLine() |> int)
+//         |> generatePlayerChoice
+//         |> playerPick
+//     let computerinput = generateComputerChoice random |> computerPick
+//     let scoreoutcome = outcome playerinput computerinput
+//     let updatedScore = updateScore initialScore scoreoutcome
+//     printScore updatedScore
+//     updatedScore
+//     
+
+let calculatePercentages score =
+    let totalGames = float score.Rounds
     let playerPercentage = (float score.Player / totalGames) * 100.0
     let computerPercentage = (float score.Computer / totalGames) * 100.0
-    { PlayerPercentage = playerPercentage; ComputerPercentage = computerPercentage }
+    let tiePercentage = ((float score.Rounds - float score.Player - float score.Computer)/ totalGames) * 100.0
+    { PlayerPercentage = playerPercentage; ComputerPercentage = computerPercentage; TiePercentage = tiePercentage }
 
 let scoreGUI score stats =
     printfn $" Player score {score.Player} and percentage {stats.PlayerPercentage}%%" 
-    printfn $" Computer score {score.Computer} and percentage {stats.ComputerPercentage}%%" 
+    printfn $" Computer score {score.Computer} and percentage {stats.ComputerPercentage}%%"
+    printfn $" Rounds: {score.Rounds} Tie percentage {stats.TiePercentage}%%"
+
 let scoreBoard score =
     printfn "Game Over! "
-    let totalPossible = 5 // Set the total possible games here
-    calculatePercentages score totalPossible
+    calculatePercentages score
     |> scoreGUI score
     |> fun _ ->
         match score.Player with
@@ -145,28 +194,30 @@ let scoreBoard score =
     
 
     
-let rec gameLoop (score: Score, random: Random) =
-        match score.Rounds with
-        | 5 -> scoreBoard score 
-        | _ ->
-            let userInput = Console.ReadLine()
-            match userInput with
-            | "q" -> scoreBoard score
-            | _ ->
-            let updatedScore = round (score,random) 
-            gameLoop (updatedScore, random)
+let rec gameLoop action scoreboard random =
+        match action with
+        | PlayRound -> 
+            playRound scoreboard random
+            |> fun ( newAction, newScoreboard) -> gameLoop newAction newScoreboard random
+        | MaxRounds | Quit -> 
+            scoreGUI scoreboard (calculatePercentages scoreboard)
+            |> (fun _ -> 0)
             
 let runGame =
-    let initialScore = { Player = 0; Computer = 0; Rounds = 0}
-    let random = Random()
-    gameLoop (initialScore, random)
+    let action = Action.PlayRound
     
+    let initialScore = { Player = 0; Computer = 0; Rounds = 1}
+    let random = Random()
+    gameLoop action initialScore random
+
+
 
 [<EntryPoint>]
 
 let main _ =
         
         runGame
+     
         // // let playerinput = playerChoose
         // // playerPick playerinput
         // let playerinput = playerChoice
