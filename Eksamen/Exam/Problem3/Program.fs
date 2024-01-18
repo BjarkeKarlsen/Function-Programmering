4953cd1100aa10194acbefda7c7b0226201b5336
Mythanhthile <93250566+Mythanhthile@users.noreply.github.com>
2024-01-18 19:08:11 +0100

    print implementation

--- Eksamen/Exam/Problem3/Program.fs
+++ Eksamen/Exam/Problem3/Program.fs
@@ -2,6 +2,7 @@ module Problem3.Program
 open System
 open FParsec
 open Problem3.Parsers
+open Problem3.Type
 
 [<EntryPoint>]
 let main argv =
@@ -9,8 +10,19 @@ let main argv =
     
     let inputString = "2019-01-29T17:57:25.000Z,4957.0,40861.16"
     
-    let print 
-        
+    let print (exercises: Exercises) (measurements: Measurement list) =
+        printfn "TrainingLoad:"
+        printfn $"StartTime: {exercises.StartTime}"
+        printfn $"TotalTime: {exercises.TotalTime}"
+        printfn $"Distance: {exercises.Distance}"
+        printfn "Measurements:"
+        measurements |> List.iter (fun m ->
+            printfn $"Time: {m.Time}"
+            printfn $"DistanceTraveled: {m.DistanceTraveled}"
+            printfn $"Speed: {m.Speed}"
+            printfn $"Cadence: {m.Cadence}"
+            printfn $"Watt: {m.Watt}")
+    
     match run exercisesParser inputString with
         | Success(result, _, _) -> 
             printfn "Parsed result: %A" result
